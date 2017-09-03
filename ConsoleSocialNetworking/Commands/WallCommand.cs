aafaa640f0705367b383df09a62cc169375f820e
Christina <Christina@MSI>
2017-09-03 11:38:29 +0100

    Testing WallCommand Output, Adding Output to WallCommand

--- ConsoleSocialNetworking/Commands/WallCommand.cs
+++ ConsoleSocialNetworking/Commands/WallCommand.cs
@@ -8,14 +8,14 @@ namespace ConsoleSocialNetworking.Commands
     public class WallCommand : Command
     {
         private readonly Deck _deck;
-        private IOutputWriter _testWriter;
+        private IOutputWriter _writer;
         private readonly string _userName;
 
         public WallCommand(Deck deck, string commandString, IOutputWriter writer) : base(commandString)
         {
             _deck = deck;
             _userName = commandString.Split(new[] {" wall"}, StringSplitOptions.RemoveEmptyEntries)[0];
-            _testWriter = writer;
+            _writer = writer;
         }
 
         public override string RegexPattern { get; } = @"\w+ wall";
@@ -25,6 +25,10 @@ namespace ConsoleSocialNetworking.Commands
         public override void Execute()
         {
             Posts = _deck.Wall(_userName);
+            foreach (var post in Posts)
+            {
+                _writer.WriteLine($"{post.UserName} - {post.GetMessageString()}");
+            }
         }
     }
 }
--- ConsoleSocialNetworkingTests/CommandTests/WallCommandTests.cs
+++ ConsoleSocialNetworkingTests/CommandTests/WallCommandTests.cs
@@ -35,11 +35,30 @@ namespace ConsoleSocialNetworkingTests.CommandTests
             postCommand.Execute();
             postCommand = new PostCommand(deck, "Alice -> Two Post");
             postCommand.Execute();
-            var followCommand = new FollowCommand(deck,"Bob follows Alice");
+            var followCommand = new FollowCommand(deck,"Bob follows Alice"); 
             followCommand.Execute();
             var wallCommand = new WallCommand(deck, "Bob wall",testWriter);
             wallCommand.Execute();
             Assert.AreEqual(2, wallCommand.Posts.Count);
+        }
+
+        [TestMethod]
+        public void TestWallCommandOutput()
+        {
+
+            var testWriter = new TestWriter();
+            var deck = new Deck();
+            var postCommand = new PostCommand(deck, "Alice -> One Post");
+            postCommand.Execute();
+            postCommand = new PostCommand(deck, "Alice -> Two Post");
+            postCommand.Execute();
+            var followCommand = new FollowCommand(deck, "Bob follows Alice");
+            followCommand.Execute();
+            var wallCommand = new WallCommand(deck, "Bob wall", testWriter);
+            wallCommand.Execute();
+            Assert.AreEqual(2, testWriter.WrittenLines.Count);
+            Assert.AreEqual("Alice - One Post (just now)",testWriter.WrittenLines[0]);
+            Assert.AreEqual("Alice - Two Post (just now)", testWriter.WrittenLines[1]);
         }
         
     }
